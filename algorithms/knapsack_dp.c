2e954d2d993dec9ffd1ff3e843278e9c92ad087c
anirudh-248 <anirudhphaniraj1@gmail.com>
2024-07-15 06:32:51 +0530

    updated knapsack solutions

--- algorithms/knapsack_dp.c
+++ algorithms/knapsack_dp.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-int n,m,w[10],p[10];
+int n,m,w[100],p[100];
 
 int max(int a, int b) {
     return (a>b?a:b);
 }
 
 void knapsack_dp() {
-    int v[11][11],i,j;
+    int v[100][100],i,j;
     for (i=0; i<=n; i++) {
         for (j=0; j<=m; j++) {
             if (i==0 || j==0)
-                v[i][j] = 0;
-            else if (j<w[i-1])
+                v[i][j] == 0;
+            else if (j<w[i])
                 v[i][j] = v[i-1][j];
             else
-                v[i][j] = max(v[i-1][j], p[i-1]+v[i-1][j-w[i-1]]);
+                v[i][j] = max(v[i-1][j], p[i]+v[i-1][j-w[i]]);
         }
     }
     for (i=0; i<=n; i++) {
@@ -23,13 +23,12 @@ void knapsack_dp() {
         printf("\n");
     }
     printf("Items included: ");
-    i = n; j = m;
-    while (i>0) {
-        if (v[i][j]!=v[i-1][j]) {
-            printf("%d ",i);
-            j -= w[i-1];
+    while (n>0) {
+        if (v[n][m]!=v[n-1][m]) {
+            printf("%d ",n);
+            m -= w[n];
         }
-        i--;
+        n--;
     }
     printf("\n");
 }
@@ -40,10 +39,10 @@ void main() {
     printf("Enter the capacity of the knapsack: ");
     scanf("%d",&m);
     printf("Enter the weights of the items: ");
-    for (int i=0; i<n; i++)
+    for (int i=1; i<=n; i++)
         scanf("%d",&w[i]);
     printf("Enter the profits of the items: ");
-    for (int i=0; i<n; i++)
+    for (int i=1; i<=n; i++)
         scanf("%d",&p[i]);
     knapsack_dp();
 }
--- algorithms/knapsack_greedy.c
+++ algorithms/knapsack_greedy.c
@@ -1,58 +1,44 @@
-/*7. Design and implement C/C++ Program to solve discrete Knapsack and continuous Knapsack
-problems using greedy approximation method.*/
-
-#include<stdio.h>
-
+#include <stdio.h>
 int n,m,p[10],w[10];
-void greedy_knapsack()
-{
+
+void greedy_knapsack() {
     float max, profit=0, x[10]={0};
     int k=0,i,j;
-    for(i=0;i<n;i++)
-    {
+    for(i=0;i<n;i++) {
         max=0;
-	    //choose the item which has highest price to weight ratio
-        for(j=0;j<n;j++)
-        {
-            if(((float)p[j])/w[j] > max)
-            {
+        for(j=0;j<n;j++) {
+            if(((float)p[j])/w[j] > max) {
                 k=j;
                 max=((float)p[j])/w[j];
             }
         }
-	    //kth element has highest price to weight ratio
-        if(w[k] <= m )
-        {
+        if(w[k] <= m) {
 			x[k]=1.0;
             m = m - w[k];
             profit=profit+p[k];
             p[k]=0;
         }
-        else
-        {
-            x[k]=((float)m)/w[k];//unable fit item k into knapsack
+        else {
+            x[k]=((float)m)/w[k];
             break;
     	}
     }
-    printf("-----------------------\n");
-    printf("Discrete Knapsack Solution:\n");
-    printf("Items included are:\n");
+    printf("\nDiscrete Knapsack Solution:\n");
+    printf("Items included are: ");
     for(i=0;i<n;i++)
     	if(x[i]==1.0)
     		printf("%d ",i+1);
     printf("\nprofit = %f\n",profit);
-    printf("-----------------------------\n");
-    printf("Continuous Knapsack Solution:\n");
+    printf("\nContinuous Knapsack Solution:\n");
     printf("Items included are:\n");
     for(i=0;i<n;i++)
     	if(x[i])
     		printf("Item %d, Portion selected %f\n",i+1,x[i]);
-	profit = profit + x[k] * p[k];
+	profit += x[k] * p[k];
     printf("profit = %f\n",profit);
 }
         
-int main() 
-{
+void main() {
     int i;
 	printf("Enter the no. of items: ");
     scanf("%d",&n);
